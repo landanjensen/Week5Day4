545fef4b3ecf3bdf6d07d179a76a93e50da46dad
landanjensen <113050692+landanjensen@users.noreply.github.com>
2023-02-20 14:23:12 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+$(".navItem").css("color", "white");
+
+$("#nav").css("background-color", "#145");
+
+$("#list li:even").css("background-color", "#ccc");
+
+$("#list li:odd").css("background-color", "#aaa");
+
+$("#myInput").val("John");
